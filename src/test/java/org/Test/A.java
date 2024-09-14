4cf01557a83b6934736c265644ba92e0de206b2d
Nalini <nandynandy.240799@gmail.com>
2024-09-14 11:33:57 +0530

    hai

---
+++ src/test/java/org/Test/A.java
@@ -0,0 +1,13 @@
+package org.Test;
+
+public class A {
+private void company() {
+System.out.println("compName");
+}
+private void sample() {
+System.out.println("sampName");
+}
+private void employee() {
+System.out.println("empName");
+}
+}
